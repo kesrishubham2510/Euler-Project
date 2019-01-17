88159baf50449a298541aae9978d701f13884fdb
shubham kesri <shubhamkeshari90@gmail.com>
2019-01-17 20:15:06 +0530

    Update e5.java

--- e5.java
+++ e5.java
@@ -1,28 +1,17 @@
-package euler;
-//design a program which tells the lcm of first n integers;
+package euler
 public class e5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int r=0;
-		long t;  //2520 is The smallest divisible by each and every number between 1 tom 10;
-	 System.out.println("programme initiated....");
-			for(t=2521;r!=19;t++)
-				{ 
-				   
-				  for(int d=2;d<=20;d++)
-				  {    
-				       if(t%d==0)
-					  r++;
-	                  
-					  else
-					  {        r=0;
-						  break;
-				          }
-				  }
-                        	}
-			System.out.println("The smallest divisible by each and every number between 1 tom 20 is "+(t-1));
-
+		long n=20;
+		System.out.println(lcm(n));
 	}
-
+	public static long lcm(long n)
+	{
+		long ans=1;
+		for(int i=1;i<=n;i++)
+		{
+			ans=(ans*i)/(gcd(ans,i));			
+		}
+		return ans;
+       }
 }
