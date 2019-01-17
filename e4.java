a2c5b6453d4f45d303db10643eb64a877346734a
shubham kesri <shubhamkeshari90@gmail.com>
2019-01-17 20:03:34 +0530

    Update e4.java

--- e4.java
+++ e4.java
@@ -5,41 +5,21 @@ public class e4 {
 	public static void main(String[] args) {
 		
 		System.out.println("The largest pallindrome is ");
-     int g=0;
-     long a,b,c,z;
-     long []p=new long[812000];
-     for(a=100;a<1000;a++)
+     
+     long a,g=0,b,c,z;
+        for(a=100;a<1000;a++)
      {                                                       
     	for(b=999;b>=100;b--)
     	 {c=a*b;
     	 boolean d=checkPallindrome(c);
     	 if(d==true)
-    	 {
-    		p[g]=c;
-    		g++;
+    	 {   if(c>g)
+    		g=c;
     	 }
     	 }
      }
-    System.out.println();
-     z=max(p);
-     System.out.println("The maximum pallindrome is "+z);
-     
-	}
-
-	private static long max(long[] p) {
-		
-		long max=(long) 0;
-		for(int f=0;f<p.length-1;f++)
-		{
-			if(p[f]>max)
-				max=p[f];
-			else 
-				max=max;
-						
-		}
-		return max;
-	}
-
+     System.out.println("The maximum pallindrome is "+g);
+     	}
 	private static boolean checkPallindrome(long c) {
 		
 		long e=(long)0,t,k=c;
